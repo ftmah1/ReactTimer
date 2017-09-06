508bb9bf435ccd4cc26d80e27b2675480e8f86e3
tanwir11 <tanwir11@bitbucket.org>
2017-09-06 15:13:27 +1000

    Initial Commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,21 @@
+var React= require('react');
+var Nav = require('Nav');
+
+
+
+
+var Main = (props) => {
+  return(
+    <div> 
+      <Nav/>
+      <div>
+      <p>Main.jsx Rendered</p>
+      </div>
+    </div>
+
+  );
+
+}
+
+
+module.exports = Main;
